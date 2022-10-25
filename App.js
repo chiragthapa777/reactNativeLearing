3cf92d6259489a4505b16a905054f082050147aa
chiragthapa777 <chiragthapa777@gmail.com>
2022-10-25 17:08:12 +0545

    scroll view flatlist sectionlist

--- App.js
+++ App.js
@@ -1,43 +1,93 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button, Linking, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Linking,
+  TextInput,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [text, setText] = useState('');
-  const [click, setclick] = useState(0);
-  const [number, setNumber] = useState(0);
-  const handleSet = () => {
-    setName(text);
-  };
-  const handleAdd = () => {
-    setclick(click + 1);
-    setNumber(number + 5);
+  const [items, setItems] = useState([
+    {id: '1', name: 'chirag'},
+    {id: '2', name: 'ram'},
+    {id: '3', name: 'meera'},
+    {id: '4', name: 'purnima'},
+    {id: '5', name: 'sita'},
+    {id: '6', name: 'gita'},
+  ]);
+  const [laoding, setloading] = useState(false);
+  const handleRefresh = () => {
+    setloading(true);
+    setTimeout(() => {
+      setItems([
+        ...items,
+        {id: '7', name: 'hari'},
+        {id: '8', name: 'shyam'},
+        {id: '9', name: 'john'},
+        {id: '10', name: 'michale'},
+        {id: '11', name: 'richard'},
+        {id: '12', name: 'joey'},
+      ]);
+      setloading(false);
+    }, 3000);
   };
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>My name is {name}</Text>
-      <TextInput value={text} onChangeText={setText} placeholder="Enter Name" />
-      <Button title="set name" onPress={handleSet} />
-      <Text style={styles.text}>Exercise</Text>
-      <TextInput value={text} onChangeText={setText} placeholder="Enter Name" />
-      <Text style={styles.text}>{number}</Text>
-      <Button title="ADD" onPress={handleAdd} />
-      <Text style={styles.text}>Number of click {click}</Text>
-    </View>
+    <FlatList
+      data={items}
+      refreshControl={
+        <RefreshControl
+          refreshing={laoding}
+          onRefresh={handleRefresh}
+          colors={['blue']}
+        />
+      }
+      style={styles.body}
+      renderItem={({item}) => (
+        <View style={styles.item} key={item.id}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
+    />
+    // <ScrollView
+    //   style={styles.body}
+    // refreshControl={
+    //   <RefreshControl
+    //     refreshing={laoding}
+    //     onRefresh={handleRefresh}
+    //     colors={['blue']}
+    //   />
+    //   }>
+    //   {items.length > 0 &&
+    //     items.map(item => (
+    // <View style={styles.item} key={item.id}>
+    //   <Text style={styles.text}>{item.name}</Text>
+    // </View>
+    //     ))}
+    // </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'red',
   },
   text: {
-    color: '#ffffff',
-    fontSize: 30,
+    fontSize: 35,
+  },
+  item: {
+    alignItems: 'center',
+    fontSize: 50,
     fontWeight: 'bold',
+    backgroundColor: 'pink',
+    margin: 5,
+    padding: 15,
+    paddingVertical: 60,
   },
 });
 
