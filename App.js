f14285f1f5a483a2b748b985725e3b2c3178f2ea
chiragthapa777 <chiragthapa777@gmail.com>
2022-10-25 12:12:37 +0545

    useState

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {View, Text, StyleSheet, Button, Linking} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, Linking, TextInput} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const [text, setText] = useState('');
+  const [click, setclick] = useState(0);
+  const [number, setNumber] = useState(0);
+  const handleSet = () => {
+    setName(text);
+  };
+  const handleAdd = () => {
+    setclick(click + 1);
+    setNumber(number + 5);
+  };
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>Hello React Native</Text>
-      <Button
-        title="youtube"
-        onPress={() => {
-          Linking.openURL('https://www.youtube.com/');
-        }}
-      />
+      <Text style={styles.text}>My name is {name}</Text>
+      <TextInput value={text} onChangeText={setText} placeholder="Enter Name" />
+      <Button title="set name" onPress={handleSet} />
+      <Text style={styles.text}>Exercise</Text>
+      <TextInput value={text} onChangeText={setText} placeholder="Enter Name" />
+      <Text style={styles.text}>{number}</Text>
+      <Button title="ADD" onPress={handleAdd} />
+      <Text style={styles.text}>Number of click {click}</Text>
     </View>
   );
 };
